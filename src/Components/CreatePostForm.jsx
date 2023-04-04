db8e1feb9b939d43c18c3aa3474a43911df06e45
sabine-ludwig <125092312+sabine-ludwig@users.noreply.github.com>
2023-04-04 09:31:52 -0700

    added component files

---
+++ src/Components/CreatePostForm.jsx
@@ -0,0 +1,8 @@
+// Within the CreatePostForm component create a form with 2 input fields. 
+// There should be an input field for the user’s name, as well as the body of their post, 
+// and each should be bound to its own state variable.
+
+// Implement a handleSubmit method for your form, which should pass the entered 
+// values to a function on the App.jsx component. That function should create a new 
+// post object, add it to the existing array of posts, and update the App.js 
+// state variable with the new array.
---
+++ src/Components/Post.jsx
@@ -0,0 +1,14 @@
+// Within the Post component create a template of how you 
+// would like each individual post to be displayed. 
+// This will include the user’s name, the body of the post, and the “like” and “dislike” buttons.
+
+// Lastly, you will need to build out the “like” and “dislike” functionality. 
+// Within the Post component, create 2 separate functions – one for when the “like” 
+// button is clicked, and one for when the “dislike” button is clicked. Bind these 
+// functions to their respective buttons onClick event. Within these functions, 
+// start to lay out the steps that need to occur when each button is clicked.
+
+// 💡 Note: this user story may be a bit more complex than it seems on the surface! 
+// This will require some creative thinking and problem-solving. Be sure to review 
+// the Changing Styles on Button Click demo video for more information about how 
+// to approach this user story!
